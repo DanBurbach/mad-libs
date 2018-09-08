ff97c0cfbeacb2d24a981c001abd444118015c7c
DanBurbach <41841936+DanBurbach@users.noreply.github.com>
2018-09-08 14:34:22 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var adjective1 = $("input#adjective1").val();
+    var adjective2 = $("input#adjective2").val();
+    var animal1= $("input#animal1").val();
+    var room = $("input#room").val();
+    var verbpast1 = $("input#verbpast1").val();
+    var verb1 = $("input#verb1").val();
+    var person1 = $("input#person1").val();
+    var noun = $("input#noun").val();
+    var liquid = $("input#liquid").val();
+    var verbending = $("input#verbending").val();
+    var bodypart = $("input#bodypart").val();
+    var pluralnoun = $("input#pluralnoun").val();
+    var noun2 = $("input#noun2").val();
 
+    $(".adjective1").text(adjective1Input);
+    $(".adjective2").text(adjective2Input);
+    $(".animal1").text(animal1Input);
+    $(".room").text(roomInput);
+    $(".verbpast1").text(verbpast1Input);
+    $(".verb1").text(verb1Input);
     $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
     $(".noun").text(nounInput);
+    $(".liquid").text(liquidInput);
+    $(".verbending").text(verbendingInput);
+    $(".bodypart").text(bodypartInput);
+    $(".pluralnoun").text(pluralnounInput);
+    $(".noun2").text(noun2Input);
 
     $("#story").show();
 
